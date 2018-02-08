653a856305540322bb3b91e5287e490f4211f4d5
Zachary Harwood <zpharwood@gmail.com>
2018-02-07 19:22:30 -0500

    got basic go endpoint working

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world")
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	http.ListenAndServe(":8080", nil)
 }
